76b50688dda8574d05e06f01d2bb1c73deb0829b
HikariB <hebiao@zju.edu.cn>
2019-07-19 11:40:23 +0800

    chatroom

---
+++ src/test/java/com/hb/websocket2demo/Websocket2DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hb.websocket2demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Websocket2DemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
